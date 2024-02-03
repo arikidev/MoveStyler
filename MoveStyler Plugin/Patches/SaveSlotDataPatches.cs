3606044ec932556a362e6e6df64852f2c939aab0
Ariki <ariki.hodgson@gmail.com>
2024-02-03 22:45:02 +0000

    interim solution to stop game trying to save custom movestyles.

--- MoveStyler Plugin/Patches/SaveSlotDataPatches.cs
+++ MoveStyler Plugin/Patches/SaveSlotDataPatches.cs
@@ -1,10 +1,41 @@
 ﻿using HarmonyLib;
 using Reptile;
 using System;
+using System.IO;
 
 namespace MoveStyler.Patches
 {
-   /* 
+    [HarmonyPatch(typeof(Reptile.CharacterProgress), nameof(Reptile.CharacterProgress.Write))]
+    public class CharacterProgressPatch
+    {
+        public static bool Prefix( ref CharacterProgress __instance)
+        {
+            MoveStyle movestyle = (MoveStyle)__instance.GetField("moveStyle").GetValue(__instance);
+
+            if (movestyle > MoveStyle.MAX)
+            {
+                __instance.GetField("moveStyle").SetValue(__instance, MoveStyle.BMX);
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Reptile.CharacterProgress), nameof(Reptile.CharacterProgress.Read))]
+    public class CharacterProgressReadPatch
+    {
+        public static void Postfix( ref CharacterProgress __instance)
+        {
+            MoveStyle movestyle = (MoveStyle)__instance.GetField("moveStyle").GetValue(__instance);
+
+            if (movestyle > MoveStyle.MAX)
+            {
+                __instance.GetField("moveStyle").SetValue(__instance, MoveStyle.BMX);
+            }
+        }
+    }
+
+    /* 
     [HarmonyPatch(typeof(Reptile.SaveSlotData), nameof(Reptile.SaveSlotData.GetCharacterProgress))]
     public class SaveSlotGetProgressPatch
     {
@@ -31,5 +62,5 @@ namespace MoveStyler.Patches
         }
     }
    */
-    
+
 }
